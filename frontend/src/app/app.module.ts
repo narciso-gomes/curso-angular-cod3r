2cc2808ae87ef19bf79d55991028d8ffc4beba74
Narciso Gomes <narcisoalmeida25@gmail.com>
2021-08-07 21:37:16 -0300

    tabela de listagem de produtos

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {NgModule, LOCALE_ID} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -20,6 +20,16 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
+import {ProductReadComponent} from './components/product/product-read/product-read.component';
+import {ProductsRead2Component} from './components/product/products-read2/products-read2.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common'
+
+registerLocaleData(localePt)
 
 
 @NgModule({
@@ -30,7 +40,9 @@ import {MatInputModule} from "@angular/material/input";
         NavComponent,
         HomeComponent,
         ProductCrudComponent,
-        ProductCreateComponent
+        ProductCreateComponent,
+        ProductReadComponent,
+        ProductsRead2Component
     ],
     imports: [
         BrowserModule,
@@ -45,9 +57,14 @@ import {MatInputModule} from "@angular/material/input";
         HttpClientModule,
         FormsModule,
         MatFormFieldModule,
-        MatInputModule
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule
     ],
-    providers: [],
+    providers: [
+        {provide: LOCALE_ID, useValue: 'pt-BR'}
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
